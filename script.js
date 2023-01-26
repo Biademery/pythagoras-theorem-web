b895a0e9cd29f472d4c33b19f3bdce34f72e602b
Bia D'Emery <biademery@gmail.com>
2023-01-26 16:14:58 -0300

    fix the problem

--- script.js
+++ script.js
@@ -16,24 +16,19 @@ button.addEventListener('click', event => {
   };
 
   fetch('https://aqueous-river-66079.herokuapp.com/', {
-    mode: 'no-cors',
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
   })
-    .then(response => {
-      console.log(response);
-      response.json();
-    })
+    .then(response => response.json())
     .then(responseData => {
-      console.log(responseData);
       if (!data.hypotenuse) fields.hypotenuse.value = responseData;
       if (!data.opposite) fields.opposite.value = responseData;
       if (!data.adjacent) fields.adjacent.value = responseData;
+    })
+    .catch(() => {
+      alert('Invalid values!');
     });
-  // .catch(() => {
-  //   alert('Invalid values!');
-  // });
 });
